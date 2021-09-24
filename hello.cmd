ac67f16861b5bf24a30f2dc7d706b192965e708a
niicat <90927498+niicat@users.noreply.github.com>
2021-09-24 13:49:32 -0400

    Update hello.cmd
    Proposed edits.

--- hello.cmd
+++ hello.cmd
@@ -1,8 +1,5 @@
-echo off
+@echo off
 echo Hi! This is a sample github project! 
-echo off
-echo In this line and the next change two words to something else. 
-echo off
-echo Be sure to test your file when you're done!
-echo off
+echo In this line and the next change some words to something else. 
+echo Be sure to save this file when you're done!
 pause
