325b17f5226a7bdc6865807995db563089b95aea
niicat <90927498+niicat@users.noreply.github.com>
2021-09-24 13:40:38 -0400

    Update hello.cmd
    This can be cleaned up and consolidated

--- hello.cmd
+++ hello.cmd
@@ -1,8 +1,5 @@
-echo off
+@echo off
 echo Hi! This is a sample github project! 
-echo off
 echo In this line and the next change two words to something else. 
-echo off
 echo Be sure to save your file as hello-test.cmd when you're done!
-echo off
 pause
