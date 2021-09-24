7b3e6231e79a09ca1d4f070dd21830ac0cef3e50
niicat <90927498+niicat@users.noreply.github.com>
2021-09-24 13:44:54 -0400

    Update hello.cmd

--- hello.cmd
+++ hello.cmd
@@ -3,6 +3,6 @@ echo Hi! This is a sample github project!
 echo off
 echo In this line and the next change two words to something else. 
 echo off
-echo Be sure to save your file as hello-test.cmd when you're done!
+echo Be sure to test your file when you're done!
 echo off
 pause
