3a148a0043371886b4f2036201bd068a3b994ac4
niicat <90927498+niicat@users.noreply.github.com>
2021-09-24 13:19:41 -0400

    Add files via upload
    Test file, feel free to edit!

---
+++ hello.cmd
@@ -0,0 +1,8 @@
+echo off
+echo Hi! This is a sample github project! 
+echo off
+echo In this line and the next change two words to something else. 
+echo off
+echo Be sure to save your file as hello-test.cmd when you're done!
+echo off
+pause
